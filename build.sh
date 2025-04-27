21f3cf0203f3d51e01901c8921423b9e179d90f3
meganii <meganii@outlook.jp>
2025-04-27 16:05:18 +0900

    Remove

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 export GOPRIVATE=github.com/meganii/vault-content
-git config --global url."https://${GO_MODULES_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+git config --global url."https://${GO_MODULES_TOKEN}:@github.com/".insteadOf "https://github.com/"
 hugo --gc --minify
