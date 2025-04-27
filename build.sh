3a9e3c9bfa13c72d9d5b2447c00a6347d4e44a6d
meganii <meganii@outlook.jp>
2025-04-27 16:12:47 +0900

    Fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 export GOPRIVATE=github.com/meganii/vault-content
-git config --global url."https://${GO_MODULES_TOKEN}:@github.com/".insteadOf "https://github.com/"
+git config --global url."https://${GO_MODULES_TOKEN}:@github.com/meganii/vault-content/".insteadOf "https://github.com/meganii/vault-content"
 hugo --gc --minify
